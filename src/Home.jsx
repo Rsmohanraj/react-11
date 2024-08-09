0e64a693cd2ac871763cba66ed51b445b7438bbf
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-08-09 12:02:53 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div className="container" >
+      <div className="row ">
+        <div className="heading text-white p-5 mt-3 mb-3 rounded-4 ">
+         
+        </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
